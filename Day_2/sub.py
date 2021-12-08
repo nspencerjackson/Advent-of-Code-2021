7b0d26e9c4783c7d012daad0fef44640b79a0e30
nspencerjackson <nspencerjackson@gmail.com>
2021-12-09 07:58:30 +0800

    2nd part

--- Day_2/sub.py
+++ Day_2/sub.py
@@ -13,19 +13,22 @@ def cont(inCommand, inLength, inPos):
         temp += inLength
     elif inCommand == "up":
         temp -= inLength
+    elif inCommand == "aim":
+        temp *= inLength
     return temp
 
 with open("controls.txt") as f:
     l = f.readlines()
 
 num = len(l)
+tempList = []
 
 for i in range(num):
-    temp = l[i].split(" ")
+    tempList = l[i].split(" ")
     #commandList.append(temp[0])
     #lengthList.append(int(temp[1]))
-    command = temp[0]
-    length = int(temp[1])
+    command = tempList[0]
+    length = int(tempList[1])
     if (command == "forward"):
         forward = cont(command, length, forward)
     else:
@@ -35,4 +38,28 @@ print(down, "m down, ", forward, "m forward")
 
 t = down * forward
 
-print(t)
+forward = 0
+down = 0
+aim = 0
+
+print("Part 1: ", t)
+
+for i in range(num):
+    tempList = l[i].split(" ")
+    command = tempList[0]
+    length = int(tempList[1])
+    if command == "forward":
+        forward = cont(command, length, forward)
+        aim = cont("aim", length, aim)
+        down = cont("down", length, aim)
+    else:
+        aim = cont(command, length, aim)
+    print("Aim: ",aim)
+
+second = forward * down
+
+print("Second Part = ", second)
+
+f = open("answer.txt", "w")
+f.write(str(second))
+f.close()
