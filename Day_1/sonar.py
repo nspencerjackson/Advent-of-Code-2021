bbc3271839596d9e7af7da4daf16b4e69b6862c4
nspencerjackson <nspencerjackson@gmail.com>
2021-12-08 16:57:38 +0800

    works

--- Day_1/sonar.py
+++ Day_1/sonar.py
@@ -1,16 +1,22 @@
 lines = []
 temp = 0
 count = 0
+first = 0
 
-with open(sweep.txt) as f:
+with open("sweep.txt") as f:
     lines = f.readlines()
 
 for x in lines:
     xInt = int(x)
     if xInt > temp:
-        count += 1
-        temp = x
+        if first == 0:
+            first += 1
+        else:
+            count += 1
+        temp = xInt
+    elif xInt == temp:
+        temp = xInt
     else:
-        temp = x
+        temp = xInt
 
 print("The count is: ", count)
