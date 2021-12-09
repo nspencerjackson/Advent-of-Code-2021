716fe0c2eae12233355d517bca4cd4fb33586372
nspencerjackson <nspencerjackson@gmail.com>
2021-12-09 15:21:45 +0800

    more code

--- Day_3/gamma.py
+++ Day_3/gamma.py
@@ -1,7 +1,17 @@
 l = []
 gamma = []
 ep = []
+leng = 0
+countOne = 0
+countZero = 0
 
-x = int("111111", 2)
+#x = int("111111", 2)
+#print(x)
 
-print(x)
+with open("binary.txt") as f:
+    l = f.readlines()
+
+leng = len(l[0])
+
+for x in l:
+    x
