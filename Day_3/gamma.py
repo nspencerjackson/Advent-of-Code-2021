45f0de5fda73bef8a2f3921187c2947a0303daa1
nspencerjackson <nspencerjackson@gmail.com>
2021-12-10 14:04:10 +0800

    more code

--- Day_3/gamma.py
+++ Day_3/gamma.py
@@ -11,22 +11,33 @@ countZero = 0
 def opposite(inGamma):
     o = ""
     for i in range(len(inGamma)):
-        if
+        if inGamma[i] == "0":
+            o += "1"
+        else:
+            o += "0"
+    return o
+
+print("opposite of 1100 is: ", opposite("1100"))
 
 with open("binary.txt") as f:
     l = f.readlines()
 
 leng = len(l[0])
 
-for x in range(leng):
+for x in range(leng-1):
+    print("x is equal to ", x)
     for y in l:
         if y[x] == "0":
             countZero += 1
         else:
             countOne += 1
     if countZero > countOne:
-        gamma += str(countZero)
+        gamma += "0"
     else:
-        gamma += str(countOne)
+        gamma += "1"
+    countZero = 0
+    countOne = 0
 
 ep = opposite(gamma)
+print("length of gamma: ", len(gamma))
+print("Gamma = ", gamma, ", and Ep= ", ep)
