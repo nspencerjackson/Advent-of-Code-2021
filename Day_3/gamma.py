6397e6f3e3b8dbc8e9ef48a8e9bd625597b81a20
nspencerjackson <nspencerjackson@gmail.com>
2021-12-16 16:02:12 +0800

    Recursion is faulty

--- Day_3/gamma.py
+++ Day_3/gamma.py
@@ -50,36 +50,38 @@ print("total = ", total)
 
 #### PART 2 ####
 
-def recursion(inArray, inBool, bitLoc):
+def recursion(inArray, inDefault, bitLoc):
     outArray = []
     newBitLoc = bitLoc
     leng = len(inArray)
     if leng > 1:
         # go through until only 1 number left
-        #
+        outArray = commonDigit(inArray, leng, bitLoc, inDefault)
+        recursion(outArray, inDefault, bitLoc)
     else:
         outArray = inArray
     return outArray
 
-def commonDigit(inArray, inDigit, inRange, bitLoc):
+def commonDigit(inArray, inRange, bitLoc, inDefault):
+    val = []
     countOne = 0
     countZero = 0
     for i in range(inRange):
         tempV = inArray[i]
-        if tempV[inDigit] == "1":
+        if tempV[bitLoc] == "1":
             countOne += 1
         else:
             countZero += 1
     print("count 0: ", countZero ,", while count 1: ", countOne)
     if countOne > countZero:
-        val = 1
+        val = rating(inArray, inRange, bitLoc, 1)
     elif countOne == countZero:
-        val = 1
+        val = rating(inArray, inRange, bitLoc, 0)
     else:
-        val = 0
+        val = rating(inArray, inRange, bitLoc, inDefault)
     return val
 
-def rating(inArray, inRange, bitLoc):
+def rating(inArray, inRange, bitLoc, inDigit):
     tempArray = []
     tempValue = ""
     for i in range(inRange):
@@ -93,5 +95,5 @@ test = ["11110", "10110", "10111", "10101", "11100", "10000", "11001", "01101", 
 
 for o in range(4):
     # make it so inDigit doesn't exist, it is calulated by seeing what in 'o' is more popular
-    ar = commonDigit(test, 1, 11, o)
+    ar = recursion(test, 1, o)
     print(ar)
