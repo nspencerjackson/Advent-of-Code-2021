15d0716e8a403eefc2ea16573c2cd95ec1eb5d1b
nspencerjackson <nspencerjackson@gmail.com>
2021-12-31 10:38:57 +0800

    delimiter split working

--- Day_4/bingo.py
+++ Day_4/bingo.py
@@ -1,19 +1,27 @@
 from bingoboards import *
 
-ar = []
+s = "4  8 12 5"
+ar = s.split()
 
 # have classes for each bingo board
 
 x = BingoBoard(1, [[1,2,3,4],[6,7,8,9],[11,12,13,14],[5,10,15,16]], 4, 4)
 
-x.updateBingo(4)
-x.getBingo()
-x.updateBingo(8)
-x.getBingo()
-x.updateBingo(12)
-x.getBingo()
-x.updateBingo(5)
-x.getBingo()
+for i in range(4):
+    print(ar[i])
+    x.updateBingo(int(ar[i]))
+    x.getBingo()
 
-b = x.checkBingo()
-print(b)
+e = x.checkBingo()
+print(e)
+#x.updateBingo(4)
+#x.getBingo()
+#x.updateBingo(8)
+#x.getBingo()
+#x.updateBingo(12)
+#x.getBingo()
+#x.updateBingo(5)
+#x.getBingo()
+
+#b = x.checkBingo()
+#print(b)
