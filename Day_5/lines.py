b25423852f5c83d34c034f4fe2155ae355865795
nspencerjackson <nspencerjackson@gmail.com>
2022-01-09 08:19:17 +0800

    added code

--- Day_5/lines.py
+++ Day_5/lines.py
@@ -1,8 +1,19 @@
 linesArray = []
 temp = []
+arr = []
+startLine = []
+endLine = []
 
 # Opens file and reads file
 with open("vents.txt") as f:
     temp = f.readlines()
 
-if
+for i in range(len(temp)):
+    tempLine = temp[i].split("\n")
+    arr.append(tempLine[0].split(" -> "))
+
+for i in range(len(arr)):
+    startLine.append(arr[i][0].split(","))
+    endLine.append(arr[i][1].split(","))
+
+#print(arr)
